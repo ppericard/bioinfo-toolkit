bf5251e88dbbcd3034e2420cb66e08aa97a9033c
Pierre Pericard <pierre.pericard@gmail.com>
2025-03-09 15:00:11 +0800

    Add functional tests for tools and simplify logger import

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,11 +1,11 @@
 """Utility modules for bioinfo-toolkit."""
 
-from src.utils.bioinfo_logger import setup_logger, get_logger
+from src.utils.bioinfo_logger import get_logger
 from src.utils.memory_tracker import MemoryTracker
 from src.utils.fastq_utils import (
-    read_fastq_entry,
-    write_fastq_entry,
-    get_read_name,
-    is_paired_entry,
-    is_paired
+    read_fastq_records,
+    extract_read_id,
+    open_file,
+    build_read_id_set,
+    process_fastq_file
 ) 
